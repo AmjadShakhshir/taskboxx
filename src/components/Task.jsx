0d851860c8b086cd75f094bb939b6ead77c5b6f0
JoniShiro <88798170+JoniShiro@users.noreply.github.com>
2024-08-29 09:38:30 +0300

    change task background to white

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -9,9 +9,8 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
       </label>
 
       <label htmlFor={`title-${id}`} aria-label={title} className="title">
-        <input type="text" value={title} readOnly={true} name="title" id={`title-${id}`} placeholder="Input title" style={{ backgroundColor: "red" }} />
+        <input type="text" value={title} readOnly={true} name="title" id={`title-${id}`} placeholder="Input title" />
       </label>
-
       {state !== "TASK_ARCHIVED" && (
         <button className="pin-button" onClick={() => onPinTask(id)} id={`pinTask-${id}`} aria-label={`pinTask-${id}`} key={`pinTask-${id}`}>
           <span className={`icon-star`} />
@@ -20,7 +19,6 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
     </div>
   );
 }
-
 Task.propTypes = {
   /** Composition of the task */
   task: PropTypes.shape({
